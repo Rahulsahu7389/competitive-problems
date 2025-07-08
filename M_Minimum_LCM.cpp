c907b5c0aad6b06f8993077f81438e20adefa03b
Rahulsahu7389 <rahulsahuppd@gmail.com>
2025-07-08 22:14:29 +0530

    8-7-25

---
+++ M_Minimum_LCM.cpp
@@ -0,0 +1,65 @@
+#include<bits/stdc++.h>
+using namespace std; 
+
+#define FOR(a, c) for (int(a) = 0; (a) < (c); (a)++) 
+#define FORLL(a, c) for (long long(a) = 0; (a) < (c); (a)++) 
+#define FORR(a, b, c) for (int(a) = (b); (a) >= (c); (a)--) 
+typedef long long int ll; 
+typedef vector<int> vi; 
+typedef pair<int, int> pi; 
+#define all(a) a.begin(),a.end()
+#define F first 
+#define S second 
+#define pb push_back 
+#define pob pop_back 
+ll MOD = 1e9 + 7;
+
+template<typename T>
+T mod(T a){
+  if(a<0) return -a;
+  return a;
+}
+
+template<typename T>
+T power(T x,T n){
+  T pro = 1;
+  while(n!=0){
+    if(n%2==1){
+      pro *= x;
+    }
+    x *= x;
+    n /= 2;
+  }
+  return pro;
+}
+
+
+void solve(){
+  //your code starts from here
+  ll n;
+  cin>>n;
+  //square root method to find the factors
+  ll a = 1;
+  for (ll i = 2; i*i <= n; i++)
+  {
+    if(n%i == 0){
+        a = (n/i);//here a is the largest one
+        break;
+    }
+  }
+  cout<<a<<" "<<(n- a)<<endl;
+  
+
+  
+}
+int main() 
+{ 
+    ios::sync_with_stdio(0); 
+    cin.tie(0); 
+    ll T; 
+    cin >> T; 
+    while (T--) { 
+        solve(); 
+    } 
+    return 0; 
+} 
