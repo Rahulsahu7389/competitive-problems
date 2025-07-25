c4432ff343b318be7746b6a85f7657a7055e46ec
Rahulsahu7389 <rahulsahuppd@gmail.com>
2025-07-25 21:46:58 +0530

    25-7-25

---
+++ B_MEXor_Mixup.cpp
@@ -0,0 +1,87 @@
+#include<bits/stdc++.h>
+using namespace std; 
+
+#define FOR(a, c) for (int(a) = 0; (a) < (c); (a)++) 
+#define FORLL(a, c) for (long long(a) = 0; (a) < (c); (a)++) 
+#define FORR(a, b, c) for (int(a) = (b); (a) >= (c); (a)--) 
+typedef long long int ll; 
+typedef vector<int> vi; 
+typedef pair<int, int> pi; 
+#define all(a) a.begin(),a.end()
+#define F first 
+#define S second 
+#define pb push_back 
+#define pob pop_back 
+ll MOD = 1e9 + 7;
+
+template<typename T>
+T mod(T a){
+  if(a<0) return -a;
+  return a;
+}
+
+template<typename T>
+T power(T x,T n){
+  T pro = 1;
+  while(n!=0){
+    if(n%2==1){
+      pro *= x;
+    }
+    x *= x;
+    n /= 2;
+  }
+  return pro;
+}
+
+
+void solve(){
+  //your code starts from here
+  ll a,b;
+  cin>>a>>b;
+  ll ans = a;
+  ll xored = 0;
+  ll s = (a-1)%4;
+  if(s == 0){
+    xored = a-1;
+  }
+  else if(s == 1){
+    xored = 1;
+  }
+  else if(s == 2){
+    xored = a;
+  }
+  else if(s == 3){
+    xored = 0;
+  }
+//   cout<<"xored is:"<<xored<<endl;
+
+  if(xored == b){
+    cout<<a<<endl;
+  }
+  else{
+    if((xored^b) != a){
+        cout<<(a+1)<<endl;
+        return;
+    }
+    else{
+        
+        cout<<(a+2)<<endl;
+        return;
+    }
+  }
+  
+
+
+  
+}
+int main() 
+{ 
+    ios::sync_with_stdio(0); 
+    cin.tie(0); 
+    ll T; 
+    cin >> T; 
+    while (T--) { 
+        solve(); 
+    } 
+    return 0; 
+} 
