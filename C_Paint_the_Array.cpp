b76ba91fa2945c2a22ccfcb0d1e95868e2484224
Rahulsahu7389 <rahulsahuppd@gmail.com>
2025-09-08 23:06:38 +0530

    8-9-25

---
+++ C_Paint_the_Array.cpp
@@ -0,0 +1,122 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FOR(a, c) for (int(a) = 0; (a) < (c); (a)++) 
+#define FORLL(a, c) for (long long(a) = 0; (a) < (c); (a)++) 
+#define FORR(a, b, c) for (int(a) = (b); (a) >= (c); (a)--) 
+typedef long long int ll; 
+typedef vector<int> vi; 
+typedef pair<int, int> pi; 
+#define all(a) a.begin(),a.end()
+#define F first 
+#define S second 
+#define pb push_back 
+#define pob pop_back 
+ll MOD = 1e9 + 7;
+
+// ======== DEBUG SYSTEM ========
+bool DEBUG_MODE = true;  // toggle before submission
+
+template<typename T> void _print(const T &x) { cerr << x; }
+template<typename T1, typename T2> void _print(const pair<T1, T2> &p) { cerr << "{"; _print(p.first); cerr << ","; _print(p.second); cerr << "}"; }
+template<typename T> void _print(const vector<T> &v) { cerr << "["; for (auto &i : v) { _print(i); cerr << " "; } cerr << "]"; }
+template<typename T> void _print(const set<T> &s) { cerr << "{"; for (auto &i : s) { _print(i); cerr << " "; } cerr << "}"; }
+template<typename T> void _print(const multiset<T> &s) { cerr << "{"; for (auto &i : s) { _print(i); cerr << " "; } cerr << "}"; }
+template<typename K, typename V> void _print(const map<K, V> &m) { cerr << "{"; for (auto &p : m) { _print(p.first); cerr << "->"; _print(p.second); cerr << " "; } cerr << "}"; }
+
+// Variadic template for multiple args
+void dbg_out() { cerr << "\n"; }
+template<typename Head, typename... Tail>
+void dbg_out(Head H, Tail... T) { _print(H); if(sizeof...(T)) cerr << " | "; dbg_out(T...); }
+
+#define dbg(...) if(DEBUG_MODE){ cerr << "[" << __LINE__ << "] " << #__VA_ARGS__ << " = "; dbg_out(__VA_ARGS__); }
+
+// ======== UTILITY FUNCTIONS ========
+template<typename T>
+T mod(T a){ return (a<0)? -a : a; }
+
+template<typename T>
+T power(T x,T n){
+  T pro = 1;
+  while(n!=0){
+    if(n%2==1) pro *= x;
+    x *= x;
+    n /= 2;
+  }
+  return pro;
+}
+
+void solve(){
+   //your code starts from here
+   ll n;
+   cin>>n;
+   vector<ll> v(n);
+   for (ll i = 0; i < n; i++)
+   {
+    cin>>v[i];
+   }
+   ll g1 = v[0];
+   for (ll i = 2; i < n; i+=2)
+   {
+    g1 = __gcd(g1,v[i]);
+   }
+   ll g2 = v[1];
+   for (ll i = 1; i < n; i+=2)
+   {
+    g2 = __gcd(g2,v[i]);
+   }
+   if(g1 == 1 && g2 == 1){
+    cout<<0<<endl;
+    return;
+   }
+//    dbg(g1,g2)
+   //g1
+   bool flag = true;
+   if(g1 == 1){
+    flag = false;
+   }
+   for (ll i = 1; i < n; i++)
+   {
+    if(v[i-1]%g1 == v[i]%g1){
+        flag = false;
+        break;
+    }
+   }
+   bool flag2 = true;
+   if(g2 == 1){
+    flag2 = false;
+   }
+   for (ll i = 1; i < n; i++)
+   {
+    if(v[i-1]%g2 == v[i]%g2){
+        flag2 = false;
+        break;
+    }
+   }
+   if(!flag && !flag2){
+    cout<<0<<endl;
+    return;
+   }
+   else if(flag && !flag2){
+    cout<<g1<<endl;
+   }
+   else{
+    cout<<g2<<endl;
+   }
+   
+   
+
+   
+}
+
+int main() 
+{ 
+    ios::sync_with_stdio(0); 
+    cin.tie(0); 
+    ll T; 
+    cin >> T; 
+    while (T--) { 
+        solve(); 
+    } 
+    return 0; 
+}
