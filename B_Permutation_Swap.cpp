5fee38f26d1fdf5d174f87718ee82b46ff6aa43b
Rahulsahu7389 <rahulsahuppd@gmail.com>
2025-10-23 21:53:00 +0530

    23-10-25

---
+++ B_Permutation_Swap.cpp
@@ -0,0 +1,91 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FOR(a, c) for (int(a) = 0; (a) < (c); (a)++) 
+#define FORLL(a, c) for (long long(a) = 0; (a) < (c); (a)++) 
+#define FORR(a, b, c) for (int(a) = (b); (a) >= (c); (a)--) 
+typedef long long int ll; 
+typedef vector<int> vi; 
+typedef pair<int, int> pi; 
+#define all(a) a.begin(),a.end()
+#define F first 
+#define S second 
+#define pb push_back 
+#define pob pop_back 
+ll MOD = 1e9 + 7;
+
+// ======== DEBUG SYSTEM ========
+bool DEBUG_MODE = true;  // toggle before submission
+
+template<typename T> void _print(const T &x) { cerr << x; }
+template<typename T1, typename T2> void _print(const pair<T1, T2> &p) { cerr << "{"; _print(p.first); cerr << ","; _print(p.second); cerr << "}"; }
+template<typename T> void _print(const vector<T> &v) { cerr << "["; for (auto &i : v) { _print(i); cerr << " "; } cerr << "]"; }
+template<typename T> void _print(const set<T> &s) { cerr << "{"; for (auto &i : s) { _print(i); cerr << " "; } cerr << "}"; }
+template<typename T> void _print(const multiset<T> &s) { cerr << "{"; for (auto &i : s) { _print(i); cerr << " "; } cerr << "}"; }
+template<typename K, typename V> void _print(const map<K, V> &m) { cerr << "{"; for (auto &p : m) { _print(p.first); cerr << "->"; _print(p.second); cerr << " "; } cerr << "}"; }
+
+// Variadic template for multiple args
+void dbg_out() { cerr << "\n"; }
+template<typename Head, typename... Tail>
+void dbg_out(Head H, Tail... T) { _print(H); if(sizeof...(T)) cerr << " | "; dbg_out(T...); }
+
+#define dbg(...) if(DEBUG_MODE){ cerr << "[" << __LINE__ << "] " << #__VA_ARGS__ << " = "; dbg_out(__VA_ARGS__); }
+
+// ======== UTILITY FUNCTIONS ========
+template<typename T>
+T mod(T a){ return (a<0)? -a : a; }
+
+template<typename T>
+T power(T x,T n){
+  T pro = 1;
+  while(n!=0){
+    if(n%2==1) pro *= x;
+    x *= x;
+    n /= 2;
+  }
+  return pro;
+}
+
+void solve(){
+   //your code starts from here
+   ll n;cin>>n;
+   vector<ll> v(n);
+   map<ll,ll> m;
+   for (ll i = 0; i < n; i++)
+   {
+    cin>>v[i];
+    m[v[i]] = i;
+   }
+   vector<ll> temp;
+   for (ll i = 0; i < n; i++)
+   {
+    if(v[i]!=i + 1){
+        ll sum = abs(m[i+1] - i);
+        temp.pb(sum);
+    }
+   }
+   ll ans = temp[0];
+//    dbg(ans)
+//    cout<<(__gcd(3,3))<<endl;
+   for (ll i = 1; i < temp.size(); i++)
+   {
+    ans = __gcd(ans,temp[i]);
+   }
+   cout<<ans<<endl;
+//    dbg(m)
+//    dbg(temp)
+   
+   
+}
+
+int main() 
+{ 
+    ios::sync_with_stdio(0); 
+    cin.tie(0); 
+    ll T; 
+    cin >> T; 
+    while (T--) { 
+        solve(); 
+    } 
+    return 0; 
+}
--- Dice_Combinations.cpp
+++ Dice_Combinations.cpp
@@ -63,69 +63,110 @@ int main()
 
 */
 //appraoch 2 - recursive code by me
-#include "bits/stdc++.h" 
-using namespace std; 
+// #include "bits/stdc++.h" 
+// using namespace std; 
 
-#define FOR(a, c) for (int(a) = 0; (a) < (c); (a)++) 
-#define FORLL(a, c) for (long long(a) = 0; (a) < (c); (a)++) 
-#define FORR(a, b, c) for (int(a) = (b); (a) >= (c); (a)--) 
-#define INF 1000000000000000003 
-typedef long long int ll; 
-typedef vector<int> vi; 
-typedef pair<int, int> pi; 
-#define F first 
-#define S second 
-#define pb push_back 
-#define pob pop_back 
-ll MOD = 1e9 + 7;
-long long modl(ll a){
-  if(a<0){return -a;}
-  return a;
-}
+// #define FOR(a, c) for (int(a) = 0; (a) < (c); (a)++) 
+// #define FORLL(a, c) for (long long(a) = 0; (a) < (c); (a)++) 
+// #define FORR(a, b, c) for (int(a) = (b); (a) >= (c); (a)--) 
+// #define INF 1000000000000000003 
+// typedef long long int ll; 
+// typedef vector<int> vi; 
+// typedef pair<int, int> pi; 
+// #define F first 
+// #define S second 
+// #define pb push_back 
+// #define pob pop_back 
+// ll MOD = 1e9 + 7;
+// long long modl(ll a){
+//   if(a<0){return -a;}
+//   return a;
+// }
 
-int modl(int a){
-  if(a<0){return -a;}
-  return a;
-}
+// int modl(int a){
+//   if(a<0){return -a;}
+//   return a;
+// }
 
-vector<ll> dp(1000001,0);
+// vector<ll> dp(1000001,0);
 
-ll fun(ll n){
-  if(n == 0){
-    return 1;
-  }
-  if(dp[n]!=0){
-    return dp[n];
-  }
-  for (ll i = 1; i <=6; i++)
+// ll fun(ll n){
+//   if(n == 0){
+//     return 1;
+//   }
+//   if(dp[n]!=0){
+//     return dp[n];
+//   }
+//   for (ll i = 1; i <=6; i++)
+//   {
+//     if(i<=n){
+//       // cout<<"entered\n";
+//       dp[n] = (dp[n]%MOD + fun(n-i)%MOD)%MOD;
+//     }
+//   }
+//   return dp[n]%MOD;
+  
+// }
+
+
+// void solve(){
+//   //your code starts from here
+//   ll n;
+//   cin>>n;
+//   dp[0] = 1;
+//   ll value = fun(n);
+//   cout<<value<<endl;
+
+// }
+// int main() 
+// { 
+//     ios::sync_with_stdio(0); 
+//     cin.tie(0); 
+//     // ll T; 
+//     // cin >> T; 
+//     // while (T--) { 
+//         solve(); 
+//     // } 
+//     return 0; 
+// } 
+
+#include<bits/stdc++.h>
+typedef long long ll;
+using namespace std;
+
+void solve(){
+  ll n;
+  cin>>n;
+  vector<ll> v = {1,2,3,4,5,6};
+  vector<vector<ll>> dp(6,vector<ll>(n+1,0));
+  for (ll i = 0; i <=n; i++)
   {
-    if(i<=n){
-      // cout<<"entered\n";
-      dp[n] = (dp[n]%MOD + fun(n-i)%MOD)%MOD;
-    }
+    dp[0][i] = 1;
   }
-  return dp[n]%MOD;
+  for (ll i = 0; i < 6; i++)
+  {
+    dp[i][0] = 1;
+  }
+  
+  for (ll i = 1; i <6; i++)
+  {
+    for (ll sum = 1; sum <=n; sum++)
+    {
+      ll take = 0;
+      if(v[i]<=sum){
+        take = dp[i-1][sum - v[i]];
+      }
+      ll nottake = dp[i-1][sum];
+      dp[i][sum] = take + nottake;
+    }
+    
+  }
+  cout<<dp[5][n]<<endl;
+  
   
 }
 
-
-void solve(){
-  //your code starts from here
-  ll n;
-  cin>>n;
-  dp[0] = 1;
-  ll value = fun(n);
-  cout<<value<<endl;
-
+int main(){
+  solve();
+  return 0;
 }
-int main() 
-{ 
-    ios::sync_with_stdio(0); 
-    cin.tie(0); 
-    // ll T; 
-    // cin >> T; 
-    // while (T--) { 
-        solve(); 
-    // } 
-    return 0; 
-} 
