87980ee464a16967a7159972557fc03c0db80b34
Rahulsahu7389 <rahulsahuppd@gmail.com>
2025-11-26 23:44:47 +0530

    26-11-25

---
+++ C_Beautiful_XOR.cpp
@@ -0,0 +1,110 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FOR(a, c) for (int(a) = 0; (a) < (c); (a)++) 
+#define FORLL(a, c) for (long long(a) = 0; (a) < (c); (a)++) 
+#define FORR(a, b, c) for (int(a) = (b); (a) >= (c); (a)--) 
+typedef long long int ll; 
+typedef vector<int> vi; 
+typedef pair<int, int> pi; 
+#define all(a) a.begin(),a.end()
+#define F first 
+#define S second 
+#define pb push_back 
+#define pob pop_back 
+ll MOD = 1e9 + 7;
+
+// ======== DEBUG SYSTEM ========
+bool DEBUG_MODE = true;  // toggle before submission
+
+template<typename T> void _print(const T &x) { cerr << x; }
+template<typename T1, typename T2> void _print(const pair<T1, T2> &p) { cerr << "{"; _print(p.first); cerr << ","; _print(p.second); cerr << "}"; }
+template<typename T> void _print(const vector<T> &v) { cerr << "["; for (auto &i : v) { _print(i); cerr << " "; } cerr << "]"; }
+template<typename T> void _print(const set<T> &s) { cerr << "{"; for (auto &i : s) { _print(i); cerr << " "; } cerr << "}"; }
+template<typename T> void _print(const multiset<T> &s) { cerr << "{"; for (auto &i : s) { _print(i); cerr << " "; } cerr << "}"; }
+template<typename K, typename V> void _print(const map<K, V> &m) { cerr << "{"; for (auto &p : m) { _print(p.first); cerr << "->"; _print(p.second); cerr << " "; } cerr << "}"; }
+
+// Variadic template for multiple args
+void dbg_out() { cerr << "\n"; }
+template<typename Head, typename... Tail>
+void dbg_out(Head H, Tail... T) { _print(H); if(sizeof...(T)) cerr << " | "; dbg_out(T...); }
+
+#define dbg(...) if(DEBUG_MODE){ cerr << "[" << __LINE__ << "] " << #__VA_ARGS__ << " = "; dbg_out(__VA_ARGS__); }
+
+// ======== UTILITY FUNCTIONS ========
+template<typename T>
+T mod(T a){ return (a<0)? -a : a; }
+
+template<typename T>
+T power(T x,T n){
+  T pro = 1;
+  while(n!=0){
+    if(n%2==1) pro *= x;
+    x *= x;
+    n /= 2;
+  }
+  return pro;
+}
+
+vector<ll> fun(ll a){
+    vector<ll> temp;
+    ll n = a;
+    while(n!=0){
+        ll rem = n%2;
+        temp.pb(rem);
+        n /= 2;
+    }
+    reverse(all(temp));
+    return temp;
+}
+
+void solve(){
+   //your code starts from here
+   ll a,b;
+   cin>>a>>b;
+   if(a == b){
+    cout<<0<<endl;
+    return;
+   }
+   vector<ll> x = fun(a);
+   vector<ll> y = fun(b);
+   dbg(x,y)
+
+   if(a<b && (x.size()<y.size())){
+    cout<<-1<<endl;
+    return;
+   }
+//    else if(a>b && (x.size()<y.size()))not possible 
+   else{
+    ll n = x.size();
+    ll idx = 0;
+    for (ll i = 0; i < n; i++)
+    {
+        if(x[i] == 1){
+            idx = i;
+            break;
+        }
+    }
+    
+    for (int i = n - 1; i >= 0; i--)
+    {
+        
+    }
+    
+   }
+
+
+
+}
+
+int main() 
+{ 
+    ios::sync_with_stdio(0); 
+    cin.tie(0); 
+    ll T; 
+    cin >> T; 
+    while (T--) { 
+        solve(); 
+    } 
+    return 0; 
+}
