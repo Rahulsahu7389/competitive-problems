f3c07081529fc429cd61fa268215922a241e092d
Rahulsahu7389 <rahulsahuppd@gmail.com>
2025-12-14 23:11:24 +0530

    14-12-25

---
+++ D_Fibonacci_Paths.cpp
@@ -0,0 +1,110 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FOR(a, c) for (int(a) = 0; (a) < (c); (a)++) 
+#define FORLL(a, c) for (long long(a) = 0; (a) < (c); (a)++) 
+#define FORR(a, b, c) for (int(a) = (b); (a) >= (c); (a)--) 
+typedef long long int ll; 
+typedef vector<int> vi; 
+typedef pair<int, int> pi; 
+#define all(a) a.begin(),a.end()
+#define F first 
+#define S second 
+#define pb push_back 
+#define pob pop_back 
+ll MOD = 1e9 + 7;
+
+// ======== DEBUG SYSTEM ========
+bool DEBUG_MODE = true;  // toggle before submission
+
+template<typename T> void _print(const T &x) { cerr << x; }
+template<typename T1, typename T2> void _print(const pair<T1, T2> &p) { cerr << "{"; _print(p.first); cerr << ","; _print(p.second); cerr << "}"; }
+template<typename T> void _print(const vector<T> &v) { cerr << "["; for (auto &i : v) { _print(i); cerr << " "; } cerr << "]"; }
+template<typename T> void _print(const set<T> &s) { cerr << "{"; for (auto &i : s) { _print(i); cerr << " "; } cerr << "}"; }
+template<typename T> void _print(const multiset<T> &s) { cerr << "{"; for (auto &i : s) { _print(i); cerr << " "; } cerr << "}"; }
+template<typename K, typename V> void _print(const map<K, V> &m) { cerr << "{"; for (auto &p : m) { _print(p.first); cerr << "->"; _print(p.second); cerr << " "; } cerr << "}"; }
+
+// Variadic template for multiple args
+void dbg_out() { cerr << "\n"; }
+template<typename Head, typename... Tail>
+void dbg_out(Head H, Tail... T) { _print(H); if(sizeof...(T)) cerr << " | "; dbg_out(T...); }
+
+#define dbg(...) if(DEBUG_MODE){ cerr << "[" << __LINE__ << "] " << #__VA_ARGS__ << " = "; dbg_out(__VA_ARGS__); }
+
+// ======== UTILITY FUNCTIONS ========
+template<typename T>
+T mod(T a){ return (a<0)? -a : a; }
+
+template<typename T>
+T power(T x,T n){
+  T pro = 1;
+  while(n!=0){
+    if(n%2==1) pro *= x;
+    x *= x;
+    n /= 2;
+  }
+  return pro;
+}
+
+void solve(){
+   //your code starts from here
+   ll n,m;
+   cin>>n>>m;
+   vector<ll> v(n);
+   for (ll i = 0; i < n; i++)
+   {
+    cin>>v[i];
+   }
+   vector<map<ll,vector<ll>>> adj(n);//for each node what its next neigbour can have
+   map<pair<ll,ll>,ll> dp;// no of paths ending at uv
+   vector<pair<ll,pair<ll,ll>>> edges;//val - actual uv
+   for (ll i = 0; i < m; i++)
+   {
+    ll a,b;
+    cin>>a>>b;
+    a--;b--;
+    dp[{a,b}] += 1;
+    edges.push_back({v[a] + v[b],{a,b}});
+    adj[a][v[b]].pb(b);
+   }
+   sort(all(edges));
+
+   map<ll,vector<pair<ll,ll>>> temp;
+   for(auto t:edges){
+    temp[t.first].pb(t.second);
+   }
+
+   for(auto val:temp){
+    map<ll,ll> tempdp;
+    for(auto p:val.second){
+        ll v = p.second;
+        tempdp[v] += dp[p];
+    }
+    for(auto t:tempdp){
+        for(auto y:adj[t.first][t.second]){
+            dp[{t.first,y}] += t.second;
+        }
+    }
+   }
+   ll ans = 0;
+   for(auto x:dp){
+    ans += x.second;
+   }
+   cout<<ans<<endl;
+   
+
+   
+   
+}
+
+int main() 
+{ 
+    ios::sync_with_stdio(0); 
+    cin.tie(0); 
+    ll T; 
+    cin >> T; 
+    while (T--) { 
+        solve(); 
+    } 
+    return 0; 
+}
