db753e491c095b0392cb8b406df1a21c8f7f0395
Rahulsahu7389 <rahulsahuppd@gmail.com>
2026-02-19 23:43:33 +0530

    19/2/26

--- A_Forked.cpp
+++ A_Forked.cpp
@@ -1,37 +1,50 @@
-#include "bits/stdc++.h" 
-using namespace std; 
+#include <bits/stdc++.h>
+using namespace std;
 
 #define FOR(a, c) for (int(a) = 0; (a) < (c); (a)++) 
 #define FORLL(a, c) for (long long(a) = 0; (a) < (c); (a)++) 
 #define FORR(a, b, c) for (int(a) = (b); (a) >= (c); (a)--) 
-#define INF 1000000000000000003 
 typedef long long int ll; 
 typedef vector<int> vi; 
 typedef pair<int, int> pi; 
+#define all(a) a.begin(),a.end()
 #define F first 
 #define S second 
 #define pb push_back 
 #define pob pop_back 
-long long modl(ll a){
-  if(a<0){return -a;}
-  return a;
+ll MOD = 1e9 + 7;
+
+// ======== DEBUG SYSTEM ========
+bool DEBUG_MODE = true;  // toggle before submission
+
+template<typename T> void _print(const T &x) { cerr << x; }
+template<typename T1, typename T2> void _print(const pair<T1, T2> &p) { cerr << "{"; _print(p.first); cerr << ","; _print(p.second); cerr << "}"; }
+template<typename T> void _print(const vector<T> &v) { cerr << "["; for (auto &i : v) { _print(i); cerr << " "; } cerr << "]"; }
+template<typename T> void _print(const set<T> &s) { cerr << "{"; for (auto &i : s) { _print(i); cerr << " "; } cerr << "}"; }
+template<typename T> void _print(const multiset<T> &s) { cerr << "{"; for (auto &i : s) { _print(i); cerr << " "; } cerr << "}"; }
+template<typename K, typename V> void _print(const map<K, V> &m) { cerr << "{"; for (auto &p : m) { _print(p.first); cerr << "->"; _print(p.second); cerr << " "; } cerr << "}"; }
+
+// Variadic template for multiple args
+void dbg_out() { cerr << "\n"; }
+template<typename Head, typename... Tail>
+void dbg_out(Head H, Tail... T) { _print(H); if(sizeof...(T)) cerr << " | "; dbg_out(T...); }
+
+#define dbg(...) if(DEBUG_MODE){ cerr << "[" << __LINE__ << "] " << #__VA_ARGS__ << " = "; dbg_out(__VA_ARGS__); }
+
+// ======== UTILITY FUNCTIONS ========
+template<typename T>
+T mod(T a){ return (a<0)? -a : a; }
+
+template<typename T>
+T power(T x,T n){
+  T pro = 1;
+  while(n!=0){
+    if(n%2==1) pro *= x;
+    x *= x;
+    n /= 2;
+  }
+  return pro;
 }
-
-int modl(int a){
-  if(a<0){return -a;}
-  return a;
-}
-
-pair<ll,ll> leftright(ll a, ll x , ll y){
-    return {x-a,x+a};
-}
-
-pair<ll,ll> topBottom(ll a, ll x , ll y){
-    return {y-a,y+a};
-}
-
-
-
 
 void solve(){
   //your code starts from here
@@ -41,17 +54,35 @@ void solve(){
   cin>>xk>>yk;
   cin>>xq>>yq;
 
-  //taking king as reference  and first using a
-  //right and left
-  ll x1 , x2;
-  auto cord = leftright(a , xk,yk);
-  cout<<cord.first<<" "<<cord.second<<endl;
+  set<pair<ll,ll>> st = {
+ {xk+a, yk+b}, {xk+a, yk-b}, {xk-a, yk+b}, {xk-a, yk-b},
+ {xk+b, yk+a}, {xk+b, yk-a}, {xk-b, yk+a}, {xk-b, yk-a}
+};
+
+set<pair<ll,ll>> sts = {
+ {xq+a, yq+b}, {xq+a, yq-b}, {xq-a, yq+b}, {xq-a, yq-b},
+ {xq+b, yq+a}, {xq+b, yq-a}, {xq-b, yq+a}, {xq-b, yq-a}
+};
+
+  ll ans = 0;
+  // dbg(st)
+  // dbg(sts)
+  for(auto val:st){
+    if(sts.count(val)>0){
+      // dbg("chla")
+      ans++;
+      // sts.erase(val);
+      // st.erase(val);
+    }
+  }
+  cout<<ans<<endl;
 
 
 
 
 
 }
+
 int main() 
 { 
     ios::sync_with_stdio(0); 
@@ -62,4 +93,4 @@ int main()
         solve(); 
     } 
     return 0; 
-} 
+}
